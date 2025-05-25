5018b3171a5f4633c0e969912a52eb100635e85b
Williams Odunayo <williamsodunayo70@gmail.com>
2025-05-25 15:06:11 +0100

    Refactor model loading and remove unused predict_image function

--- Application.py
+++ Application.py
@@ -3,15 +3,14 @@
 import os
 import numpy as np
 import cv2 as cv
-import tensorflow as tf
-from tf.keras.models import load_model
+from tensorflow.keras.models import load_model
 from werkzeug.utils import secure_filename
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 
 # Build Flask app
 app = Flask(__name__)
 
-app.config["UPLOAD_FOLDER"] = "User_Upload"
+# app.config["UPLOAD_FOLDER"] = "User_Upload"
 os.makedirs(app.config["UPLOAD_FOLDER"], exist_ok=True)
 
 # Load the model
@@ -23,21 +22,6 @@ extensions = {"jpg", "jpeg", "png"}
 def allowed_file(filename):
     return "." in filename and filename.rsplit(".", 1)[1].lower() in extensions
 
-def predict_image(filepath):
-    image = cv.imread(filepath)
-    image = cv.resize(image, (150, 150))
-    image = cv.cvtColor(image, cv.COLOR_RGB2BGR)
-    image = image.reshape(1, 150, 150, 3)
-    pred = model.predict(image)
-    pred = np.argmax(pred, axis=1)[0]
-    if pred == 0:
-        return "Glioma Tumor"
-    elif pred == 1:
-        return "No Tumor"
-    elif pred == 2:
-        return "Meningioma Tumor"
-    else:
-        return "Pituitary Tumor"
 
 @app.route("/")
 def home():
@@ -55,25 +39,35 @@ def predict():
         if "file" not in request.files:
             return render_template("predict.html", error="No file part")
         file = request.files["file"]
+        
         if file.filename == "":
             return render_template("home.html", error="No selected file")
+        
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             filepath = os.path.join(app.config["UPLOAD_FOLDER"], filename)
             file.save(filepath)
             prediction = predict_image(filepath)
             return render_template("results.html", filename=filename, prediction=prediction) 
+        
         else:
             return render_template("home.html", error="Invalid file type")
     return render_template("home.html")
+
+
+@app.route("/results")
+def results():
+    return render_template("results.html")
+
 
 @app.route('/about')
 def about():
     return render_template('about.html')
 
-@app.route("/uploads/<filename>")
-def uploaded_file(filename):
-    return send_from_directory(app.config["UPLOAD_FOLDER"], filename)
+
+# @app.route("/uploads/<filename>")
+# def uploaded_file(filename):
+#     return send_from_directory(app.config["UPLOAD_FOLDER"], filename)
 
 
 if __name__ == "__main__":
